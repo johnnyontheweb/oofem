bbb4b2de316be752c458cf5b846b647d1fd0e4a2
Giovanni Rinaldin <giovannirinaldin@hotmail.com>
2015-10-02 16:02:16 +0200

    fixed local axes in beam3d

--- src/sm/Elements/Beams/beam3d.C
+++ src/sm/Elements/Beams/beam3d.C
@@ -487,10 +487,11 @@ Beam3d :: giveLocalCoordinateSystem(FloatMatrix &answer)
 
         help.at(3) = 1.0;         // up-vector
         // here is ly is used as a temp var
-        if ( acos(lx.dotProduct(help)) < 0.001 ) { // Check if it is vertical
+		float vert = acos(lx.dotProduct(help));
+		if (vert < 0.001 || vert > M_PI - 0.001) { // Check if it is vertical
             ly = {0., 1., 0.};
         } else {
-            ly.beVectorProductOf(lx, help);
+			ly.beVectorProductOf(lx, help);
         }
         lz.beProductOf(rot, ly);
         lz.normalize();
