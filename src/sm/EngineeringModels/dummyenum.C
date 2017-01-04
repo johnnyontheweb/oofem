f40a7aa1bbfa00fb54e47c38b46c48c4d4a23d7e
Francesco <frankpnt@hotmail.it>
2017-01-04 13:25:11 +0100

    added DummyEnum engineering model

---
+++ src/sm/EngineeringModels/dummyenum.C
@@ -0,0 +1,202 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "../sm/EngineeringModels/dummyenum.h"
+#include "domain.h"
+#include "classfactory.h"
+#include "exportmodulemanager.h"
+#include "xfem/xfemmanager.h"
+
+namespace oofem {
+REGISTER_EngngModel(DummyEnum);
+
+void
+DummyEnum :: updateYourself(TimeStep *tStep)
+{
+    
+}
+
+void
+DummyEnum :: terminate(TimeStep *tStep)
+{
+	int c = 0;
+
+	fprintf(outputStream, "\n\n\tE N U M  O U T P U T:\n\t_______________________________\n");
+
+	this->doPrint<InternalStateType>(ist, (bool)(this->writeall), &__InternalStateTypeToString);
+	this->doPrint<UnknownType>(unktype, (bool)(this->writeall), &__UnknownTypeToString);
+	this->doPrint<dofType>(doftype, (bool)(this->writeall), &__dofTypeToString);
+	this->doPrint<domainType>(matmode, (bool)(this->writeall), &__domainTypeToString);
+	this->doPrint<MaterialMode>(unktype, (bool)(this->writeall), &__MaterialModeToString);
+	this->doPrint<Element_Geometry_Type>(elgeom, (bool)(this->writeall), &__Element_Geometry_TypeToString);
+	this->doPrint<ValueModeType>(valmode, (bool)(this->writeall), &__ValueModeTypeToString);
+
+	// damn
+	fprintf(outputStream, "\n\n");
+
+	if (dofid){
+		int num = dofid;
+		fprintf(outputStream, "##############################");
+		fprintf(outputStream, "\n%s\n", typeid(DofIDItem).name());
+		fprintf(outputStream, "%d - %d  ---  ", 0, num);
+		if (writeall) {
+			fprintf(outputStream, "complete\n", 0, num);
+		}
+		else {
+			fprintf(outputStream, "filtered\n", 0, num);
+		}
+
+		for (int c = 0; c <= num; c++){
+			DofIDItem enVal = (DofIDItem)(c);
+			std::string str = __DofIDItemToString(enVal);  // c_str does not live if std::string gets destroyed
+			const char *st = str.c_str();
+			if (!writeall){
+				if (strcmp(st, "Unknown") == 0){ continue; }
+			}
+			// else write
+			fprintf(outputStream, "  %4d : ", c);
+			fprintf(outputStream, st);
+			fprintf(outputStream, "\n");
+		}
+	}
+
+	this->doPrint<CharType>(chartype, (bool)(this->writeall), &__CharTypeToString);
+	this->doPrint<MatResponseMode>(matrespmode, (bool)(this->writeall), &__MatResponseModeToString);
+	this->doPrint<MaterialMappingAlgorithmType>(matmapalgo, (bool)(this->writeall), &__MaterialMappingAlgorithmTypeToString);
+	this->doPrint<MeshPackageType>(meshpack, (bool)(this->writeall), &__MeshPackageTypeToString);
+	this->doPrint<XFEMStateType>(xfemstate, (bool)(this->writeall), &__XFEMStateTypeToString);
+
+}
+
+IRResultType
+DummyEnum :: initializeFrom(InputRecord *ir)
+// input from inputString
+{
+    IRResultType result;                // Required by IR_GIVE_FIELD macro
+
+	writeall = 1;
+	IR_GIVE_OPTIONAL_FIELD(ir, writeall, _IFT_DummyEnum_writeAll);
+
+	ist = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, ist, _IFT_DummyEnum_ist);
+
+	unktype = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, unktype, _IFT_DummyEnum_unkType);
+
+	doftype = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, doftype, _IFT_DummyEnum_dofType);
+
+	domtype = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, domtype, _IFT_DummyEnum_domainType);
+
+	matmode = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, matmode, _IFT_DummyEnum_materialMode);
+
+	elgeom = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, elgeom, _IFT_DummyEnum_elementGeometry);
+
+	valmode = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, valmode, _IFT_DummyEnum_valModeType);
+
+	dofid = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, dofid, _IFT_DummyEnum_dofIDItem);
+
+	chartype = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, chartype, _IFT_DummyEnum_charType);
+
+	matrespmode = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, matrespmode, _IFT_DummyEnum_materialResponseMode);
+
+	matmapalgo = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, matmapalgo, _IFT_DummyEnum_materialMappingAlgo);
+
+	meshpack = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, meshpack, _IFT_DummyEnum_meshPackageType);
+
+	xfemstate = 0;
+	IR_GIVE_OPTIONAL_FIELD(ir, xfemstate, _IFT_DummyEnum_xFEMStateType);
+
+    //return StructuralEngngModel :: initializeFrom(ir);
+	return IRResultType::IRRT_OK;
+}
+
+int
+DummyEnum::instanciateYourself(DataReader *dr, InputRecord *ir, const char *dataOutputFileName, const char *desc)
+{
+	OOFEMTXTDataReader *txtReader = dynamic_cast< OOFEMTXTDataReader* > (dr);
+	if (txtReader != NULL) {
+		referenceFileName = std::string(txtReader->giveDataSourceName());
+	}
+
+	bool inputReaderFinish = true;
+
+	this->coreOutputFileName = std::string(dataOutputFileName);
+	this->dataOutputFileName = std::string(dataOutputFileName);
+
+	if (this->giveProblemMode() == _postProcessor) {
+		// modify output file name to prevent output to be lost
+		this->dataOutputFileName.append(".oofeg");
+	}
+
+	if ((outputStream = fopen(this->dataOutputFileName.c_str(), "w")) == NULL) {
+		OOFEM_ERROR("Can't open output file %s", this->dataOutputFileName.c_str());
+	}
+
+	fprintf(outputStream, "%s", PRG_HEADER);
+	this->startTime = time(NULL);
+	fprintf(outputStream, "\nStarting analysis on: %s\n", ctime(&this->startTime));
+
+	fprintf(outputStream, "%s\n", desc);
+	
+	// instanciate receiver
+	this->initializeFrom(ir);
+
+	exportModuleManager->initializeFrom(ir);
+	// instanciate export module manager
+	exportModuleManager->instanciateYourself(dr, ir);
+
+	if (inputReaderFinish) {
+		ir->finish();
+	}
+
+	return 1;
+}
+
+void
+DummyEnum::solveYourself()
+{
+	this->timer.startTimer(EngngModelTimer::EMTT_AnalysisTimer);
+	this->terminate(NULL);
+}
+} // end namespace oofem
---
+++ src/sm/EngineeringModels/dummyenum.h
@@ -0,0 +1,150 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef dummyenum_h
+#define dummyenum_h
+
+#include "structengngmodel.h"
+#include "datastream.h"
+#include "oofemtxtdatareader.h"
+#include "elementgeometrytype.h"
+
+///@name Input fields for DummyEnum
+//@{
+#define _IFT_DummyEnum_Name "dummyenum"
+#define _IFT_DummyEnum_writeAll "writeall"
+#define _IFT_DummyEnum_ist "ist"
+#define _IFT_DummyEnum_unkType "unktype"
+#define _IFT_DummyEnum_dofType "doftype"
+#define _IFT_DummyEnum_domainType "domtype"
+#define _IFT_DummyEnum_materialMode "matmode"
+#define _IFT_DummyEnum_elementGeometry "elgeom"
+#define _IFT_DummyEnum_valModeType "valmode"
+#define _IFT_DummyEnum_dofIDItem "dofid"
+#define _IFT_DummyEnum_charType "chartype"
+#define _IFT_DummyEnum_materialResponseMode "matrespmode"
+#define _IFT_DummyEnum_materialMappingAlgo "matmapalgo"
+#define _IFT_DummyEnum_meshPackageType "meshpack"
+#define _IFT_DummyEnum_xFEMStateType "xfemstate"
+//@}
+
+#define to_str(s) #s
+
+namespace oofem {
+/**
+ * This class implements a dummy analysis that outputs all enum contents up to a requested id
+ * This class is more a hack than a proper EngineeringModel overrides several EngineeringModel
+ * just to allow the proper setup of the minimum number of objects required for oofem to work.
+ * It may used in an automated debugging tool for a quick capability and compatibility check.
+ * 
+ * @author Francesco Pontarin
+ */
+class DummyEnum : public StructuralEngngModel
+{
+protected:
+	int ist = 0;
+	int unktype = 0;
+	int doftype = 0;
+	int domtype = 0;
+	int matmode = 0;
+	int elgeom = 0;
+	int valmode = 0;
+	int matrespmode = 0;
+	int dofid = 0;
+	int chartype = 0;
+	int matmapalgo = 0;
+	int meshpack = 0;
+	int xfemstate = 0;
+
+	int writeall = 1;
+
+public:
+	DummyEnum(int i, EngngModel * _master = NULL) : StructuralEngngModel(i, _master) { }
+    virtual ~DummyEnum() { }
+
+    virtual void updateYourself(TimeStep *tStep);
+
+    virtual void terminate(TimeStep *tStep);
+
+	virtual int instanciateYourself(DataReader *dr, InputRecord *ir, const char *dataOutputFileName, const char *desc);
+
+    virtual IRResultType initializeFrom(InputRecord *ir);
+	virtual void postInitialize() {}
+	virtual int checkProblemConsistency() { return 1; }
+	//virtual void initStepIncrements() {};
+
+	virtual void init() {}
+
+	virtual void solveYourself();
+
+    // identification
+    virtual const char *giveClassName() const { return "DummyEnum"; }
+    virtual const char *giveInputRecordName() const { return _IFT_DummyEnum_Name; }
+
+private:
+
+	// the boolean is kept for eventual future writeall flags for each enum
+	template <typename T, typename std::enable_if<std::is_enum<T>::value>::type* = nullptr>
+	void doPrint(int num, bool all, const char* (*func)(T))
+	{
+		fprintf(outputStream, "\n\n");
+
+		if (num){
+			fprintf(outputStream, "##############################");
+			fprintf(outputStream, "\n%s\n", typeid(T).name());
+			fprintf(outputStream, "%d - %d  ---  ", 0 , num);
+			if (all) {
+				fprintf(outputStream, "complete\n", 0, num);
+			} else {
+				fprintf(outputStream, "filtered\n", 0, num);
+			}
+
+			for (int c = 0; c <= num; c++){
+				T enVal = (T)(c);
+				const char *st = (*func)(enVal);
+				// write everything only if requested, else filter out "Unknown" entries
+				if (!all){
+					if (strcmp(st, "Unknown") == 0){ continue; }
+				}
+				// else write
+				fprintf(outputStream, "  %4d : ", c);
+				fprintf(outputStream, st);
+				fprintf(outputStream, "\n");
+			}
+		}
+	}
+}; // end of class
+
+} // end namespace oofem
+#endif // dummyenum_h
