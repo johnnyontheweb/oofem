47834ce5e5673fc366d7473b0c06595bb9d6ca87
johnnyontheweb <johnnyontheweb@gmail.com>
2018-09-17 19:54:09 +0200

    corrected bug in internal forces for beam3d

--- src/sm/export/beamexportmodule.C
+++ src/sm/export/beamexportmodule.C
@@ -306,13 +306,13 @@ namespace oofem {
 							if (CLoad->giveCoordSystMode() == Load::CoordSystType::CST_Global)	compArr.rotatedWith(T, 'n');
 							FinalLoads.second.add(compArr);
 
-							// add stations inside load
-							midPoints.push_back(CLoad->startLocal);
-							double halfL = CLoad->endLocal - CLoad->startLocal;
-							midPoints.push_back(CLoad->startLocal + 0.25 * halfL);
-							midPoints.push_back(CLoad->startLocal + 0.5 * halfL);
-							midPoints.push_back(CLoad->startLocal + 0.75 * halfL);
-							midPoints.push_back(CLoad->endLocal);
+							//// add stations inside load
+							//midPoints.push_back(CLoad->startLocal);
+							//double halfL = CLoad->endLocal - CLoad->startLocal;
+							//midPoints.push_back(CLoad->startLocal + 0.25 * halfL);
+							//midPoints.push_back(CLoad->startLocal + 0.5 * halfL);
+							//midPoints.push_back(CLoad->startLocal + 0.75 * halfL);
+							//midPoints.push_back(CLoad->endLocal);
 						}
 					}
 
